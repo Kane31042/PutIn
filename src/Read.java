fc679a6512949eb5962f86eab63aea1d9aadf90c
Kane31042 <56527427+Kane31042@users.noreply.github.com>
2019-11-07 22:02:53 +0800

    Add files via upload

---
+++ src/Read.java
@@ -0,0 +1,56 @@
+import java.nio.CharBuffer;
+import java.util.Scanner;
+
+public class Read {
+    public static void main(String[] args) {
+        CharBuffer buf1=CharBuffer.allocate(1024);
+        CharBuffer buf2=CharBuffer.allocate(1024);
+        boolean isPut=true;
+        while (isPut){
+            char[] chars=putIn();
+            buf1.put(chars);
+            buf1.flip();
+            while (buf1.hasRemaining()){
+                char c=buf1.get();
+                if(c=='#'){
+                    char c1=buf1.get();
+                    switch (c1){
+                        case 'b':
+                            buf2.position(buf2.position()-1);
+                            break;
+                        case 'c':
+                            buf2.position(buf2.position()-buf1.position()+2);
+                            break;
+                        case 'o':
+                            isPut=false;
+                            buf2.position(buf2.position());
+                            buf2.put('\n');
+                            break;
+                    }
+                }else {
+                    char c1=c;
+                    buf2.put(c1);
+                }
+            }
+            buf1.clear();//清空接收下一行数据
+            buf2.put('\n');//换行
+        }
+
+        //输出
+        buf2.flip();
+        while (buf2.hasRemaining()){
+            char x=buf2.get();
+            if(x=='\n'){
+                System.out.println("");
+            } else {
+                System.out.print(x);
+            }
+        }
+    }
+    public static char[] putIn(){
+        Scanner putIn=new Scanner(System.in);
+        String put=putIn.nextLine();
+        put.toCharArray();
+        return put.toCharArray();
+    }
+}
